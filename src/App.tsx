1151e331794625ca21db9f172a6cad042eae06ca
Pavlo Surzhenko <pavel.surzhenko@icloud.com>
2023-08-02 19:29:33 +0300

    add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,11 @@
 const App = () => {
-    return <>APp</>;
+    return (
+        <p className='text-darkBlue text-3xl'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
+            praesentium minima accusamus necessitatibus deserunt a, repellat
+            nihil corrupti fugiat ad quo sequi, velit natus consectetur. Quo
+            cupiditate natus amet soluta.
+        </p>
+    );
 };
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
     content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
     theme: {
-        extend: {},
+        colors: {
+            darkBlue: '#032541',
+            lightBlue: '#01B4E4',
+        },
+        extend: {
+            fontFamily: {
+                Source: ['Source Sans 3', 'sans-serif'],
+            },
+        },
     },
     plugins: [],
 };
