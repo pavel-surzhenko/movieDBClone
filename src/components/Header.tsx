f4724629ddcbcf0eb5323944134bcb7daf996c9e
Pavlo Surzhenko <pavel.surzhenko@icloud.com>
2023-08-03 17:35:30 +0300

    responsive welcome

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,6 +3,7 @@ import logoSvg from '../assets/logo.svg';
 import MenuIcon from '../assets/MenuIcon';
 import { useState } from 'react';
 import CloseIcon from '../assets/CloseIcon';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     const [showMenu, setShowMenu] = useState(false);
@@ -10,13 +11,15 @@ const Header = () => {
     return (
         <div className='bg-darkBlue'>
             <header className='py-5 flex justify-between px-5 flex-row-reverse lg:flex-row'>
-                <img
-                    src={logoSvg}
-                    alt=''
-                    className='h-5 w-40'
-                />
+                <Link to='/'>
+                    <img
+                        src={logoSvg}
+                        alt=''
+                        className='h-5 w-40'
+                    />
+                </Link>
                 <div
-                    className='block lg:hidden relative z-10'
+                    className='block lg:hidden relative z-20'
                     onClick={() => setShowMenu((prev) => !prev)}
                 >
                     {showMenu ? <CloseIcon /> : <MenuIcon />}
@@ -27,7 +30,7 @@ const Header = () => {
                 <div
                     className={`${
                         showMenu ? 'fixed block' : ' static hidden'
-                    }  top-0 left-0 right-2/4 bottom-0 bg-darkBlue pl-12 pt-20 text-xl`}
+                    }  top-0 left-0 right-1/3 bottom-0 bg-darkBlue pl-12 pt-20 text-xl z-10`}
                 >
                     <Navigation isMobile />
                 </div>
--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -29,15 +29,17 @@ export const Welcome = () => {
 
     return (
         <section
-            className='min-h-[300px] xl:min-h-[400px] flex flex-col justify-center h-full'
+            className='min-h-[300px] lg:min-h-[400px] flex flex-col justify-center h-full'
             style={{
-                background: ` center / cover no-repeat linear-gradient(to right, rgba(3, 37, 65, 0.8) 0%, rgba(3, 37, 65, 0.5) 100%), url(${baseUrlImg}w1280${image}) center / cover no-repeat`,
+                background: ` center / cover no-repeat linear-gradient(to right, rgba(3, 37, 65, 0.8) 0%, rgba(3, 37, 65, 0.5) 100%), url(${baseUrlImg}w1280${image}) center / cover  no-repeat`,
             }}
         >
             <div className='flex flex-col px-5 justify-center h-full'>
                 <div className='flex-grow  mb-5'>
-                    <h2 className='text-5xl font-bold '>Welcome</h2>
-                    <h3 className='text-[32px] font-semibold'>
+                    <h2 className='text-2xl lg:text-5xl font-bold pb-4 md:pb-0'>
+                        Welcome
+                    </h2>
+                    <h3 className='text-lg lg:text-[32px] font-semibold'>
                         Millions of movies, TV shows and people to discover.
                         Explore now.
                     </h3>
@@ -45,15 +47,19 @@ export const Welcome = () => {
                 <div>
                     <form
                         action=''
-                        className='flex'
+                        className='flex relative z-0'
                     >
                         <input
                             className='w-full rounded-[30px] px-[20px] py-[10px] text-lg text-black/50 outline-none'
                             type='text'
-                            placeholder='Search for a movie, tv show, person....'
+                            placeholder={`${
+                                window?.innerWidth >= 1024
+                                    ? 'Search for a movie, tv show, person....'
+                                    : 'Search'
+                            }`}
                         />
                         <button
-                            className='px-[26px] py-[10px] bg-gradient-to-r from-lightGreen to-lightBlue rounded-[30px] -translate-x-1/2'
+                            className='px-[26px] py-[12px] bg-gradient-to-r from-lightGreen to-lightBlue rounded-[30px] absolute right-0'
                             type='submit'
                         >
                             Search
